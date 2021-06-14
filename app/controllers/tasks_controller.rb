6ccc83a10c0965d57a2edc585689f12981200abd
shinya.mizuno <mizuno3109@yahoo.co.jp>
2021-06-14 13:51:53 +0000

    useless_delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show,:edit,:destroy]
   
   def index
-      @task = current_user.tasks.build
+     
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
@@ -52,16 +52,15 @@ class TasksController < ApplicationController
  
   private
 
-  # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
-    def correct_user
+  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_url
+    redirect_to root_url
     end
-    end 
+  end 
 
 end
