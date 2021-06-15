1ce2df6339378482b05d4cbe04c39a29ced3cb02
shinya.mizuno <mizuno3109@yahoo.co.jp>
2021-06-15 11:36:31 +0000

    controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,10 @@ class TasksController < ApplicationController
   before_action :correct_user, only: [:show,:edit,:update,:destroy]
   
   def index
-     
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
-      @task = current_user.tasks.find_by(id: params[:id])
   end
 
   def new
@@ -17,7 +15,6 @@ class TasksController < ApplicationController
 
   def create
       @task = current_user.tasks.build(task_params)
-      
       if @task.save
          flash[:succes] = 'Taskが正常に投稿されました'
          redirect_to @task
@@ -28,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find_by(id: params[:id])
   end
 
   def update
-      @task = Task.find(params[:id])
       if @task.update(task_params)
         flash[:success] = 'Task は正常に更新されました'
         redirect_to @task
@@ -43,9 +38,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
